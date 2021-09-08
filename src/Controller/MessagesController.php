3334fd2bb1002bc4c83c65fd676944ca76b1bcd9
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2021-09-08 10:52:46 +0200

    A message can be submited

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
 use App\Entity\Room;
+use App\Form\MessageType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,9 +16,26 @@ class MessagesController extends AbstractController
     /**
      * @Route("/rooms/{room}/messages/new", name="app_messages_new", methods={"GET", "POST"})
      */
-    public function index(Room $room): Response
+    public function index(Room $room, Request $request, EntityManagerInterface $em): Response
     {
+        $message = New Message;
 
-        return $this->render('messages/new.html.twig', compact('room'));
+        $form = $this->createForm(MessageType::class, $message);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message->setRoom($room);
+
+            $em->persist($message);
+            $em->flush();
+
+            return $this->redirectToRoute('app_rooms_show', ['id' => $room->getId()]);
+        }
+
+        return $this->render('messages/new.html.twig', [
+            'room' => $room,
+            'form' => $form->createView()
+        ]);
     }
 }
--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\MessageRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=MessageRepository::class)
@@ -21,6 +22,7 @@ class Message
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank
      */
     private $content;
 
