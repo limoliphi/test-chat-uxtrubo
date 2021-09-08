0fc28832fe4b65fea20e79156f2f0c978c1bee49
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2021-09-08 10:20:35 +0200

    Modifiy MessagesController with new method and add template messages/new

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Room;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class MessagesController extends AbstractController
 {
     /**
-     * @Route("/messages", name="messages")
+     * @Route("/rooms/{room}/messages/new", name="app_messages_new", methods={"GET", "POST"})
      */
-    public function index(): Response
+    public function index(Room $room): Response
     {
-        return $this->render('messages/index.html.twig', [
-            'controller_name' => 'MessagesController',
-        ]);
+
+        return $this->render('messages/new.html.twig', compact('room'));
     }
 }
